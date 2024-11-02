f92ae6e198b93c572d531a06c193a43167055257
Micheal Callaghan <mehaul96@gmail.com>
2024-11-02 18:58:48 -0500

    Installed and initialized Sentry for monitoring.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,4 +9,5 @@ const SignIn = () => {
   )
 }
 
+
 export default SignIn
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -44,7 +44,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               <BankCard
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={`${user.firstName} ${user.lastName}`}
+                userName={user.name}
                 showBalance={false}
               />
             </div>
